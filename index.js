9ccac57395dfdc7dbf7f7aa950490e4211d8743c
Eduardo San Martin Morote <posva13@gmail.com>
2017-08-01 16:07:25 +0200

    ✨ Add more rules

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ module.exports = {
   ],
   parserOptions: {
     parser: 'babel-eslint',
+    sourceType: 'module',
   },
   rules: {
     'vue/require-v-for-key': 0,
-    'import/no-webpack-loader-syntax': 0
-  }
+    'import/no-webpack-loader-syntax': 0,
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-return-await': 2,
+    'quote-props': ['error', 'as-needed'],
+  },
 }
