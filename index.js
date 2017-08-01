765c061db04ef8af97ad82aa4efe0a4c3024fab1
Eduardo San Martin Morote <posva13@gmail.com>
2017-08-01 18:24:35 +0200

    🔨 Use eslint/recommended

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: [
+    'eslint:recommended',
     'standard',
     'plugin:vue/recommended',
   ],
@@ -11,8 +12,7 @@ module.exports = {
     'vue/require-v-for-key': 0,
     'import/no-webpack-loader-syntax': 0,
     'comma-dangle': ['error', 'always-multiline'],
-    'no-return-await': 2,
-    'quote-props': ['error', 'as-needed'],
-    'no-var': 2,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 1 : 0,
   },
 }
