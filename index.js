3fa2489b06a1f7678479608dd65014a82de9fb5a
Eduardo San Martin Morote <posva13@gmail.com>
2017-08-08 23:09:26 +0200

    ⬆️ Up dependencies + rules

--- index.js
+++ index.js
@@ -9,10 +9,32 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'vue/require-v-for-key': 0,
-    'import/no-webpack-loader-syntax': 0,
+    // eslint
     'comma-dangle': ['error', 'always-multiline'],
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'no-console': process.env.NODE_ENV === 'production' ? 1 : 0,
+    'quote-props': ['error', 'as-needed'],
+
+    // vue
+    'vue/require-v-for-key': 0,
+
+    // import
+    'import/no-webpack-loader-syntax': 0,
+    'import/default': 1,
+    'import/namespace': 2,
+    'import/no-anonymous-default-export': ['error', {
+      allowArray: true,
+      allowArrowFunction: true,
+      allowAnonymousClass: false,
+      allowAnonymousFunction: false,
+      allowLiteral: false,
+      allowObject: true,
+    }],
+    'import/extensions': ['error', 'never', {
+      js: 'never',
+      jsx: 'never',
+      json: 'never',
+      vue: 'never',
+    }],
   },
 }
